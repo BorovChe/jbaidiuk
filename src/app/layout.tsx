265ada9a3923cc7a3daf7752d67020ec5df8b18f
BorovChe <120989445+BorovChe@users.noreply.github.com>
2025-04-10 16:54:15 +0300

    fix meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,7 @@
 import "./globals.css";
 import { Raleway } from "next/font/google";
-import type { Metadata } from "next";
 import Header from "@/components/main-layout/header";
 import Footer from "@/components/main-layout/footer";
-
-export const metadata: Metadata = {
-  title: "JBaidiuk | Виглядай так, як відчуваєш",
-  // description:
-  //   "JBaidiuk — професійний барбер у стилі old school. Класичні чоловічі стрижки з характером. Мінімум слів — максимум стилю.",
-  // keywords: [
-  //   "барбер",
-  //   "JBaidiuk",
-  //   "стрижки",
-  //   "чоловічі стрижки",
-  //   "барбершоп",
-  //   "старої школи",
-  //   "класичні стрижки",
-  //   "стиль",
-  // ],
-  // openGraph: {
-  //   title: "Only Quality Haircut",
-  //   description: "Професійний барбер. Якість. Традиція. Стиль.",
-  //   url: "https://jbaidiuk.vercel.app",
-  //   siteName: "JBaidiuk",
-  //   type: "website",
-  //   images: [
-  //     {
-  //       url: "/images/hero/hero.jpg",
-  //       width: 1200,
-  //       height: 630,
-  //       alt: "JBaidiuk Barber",
-  //     },
-  //   ],
-  // },
-};
 
 const raleway = Raleway({
   subsets: ["latin"],
